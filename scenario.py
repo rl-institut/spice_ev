4a10d031c8ef297002c61a29008b110017d233f5
stefan.schirmeister <stefan.schirmeister@localiser.de>
2020-11-19 16:52:08 +0100

    scenario: fix plotting, add price plot

--- scenario.py
+++ scenario.py
@@ -42,92 +42,81 @@ class Scenario:
         event_steps = self.events.get_event_steps(self.start_time, self.n_intervals, self.interval)
 
         costs = []
+        prices = []
         results = []
-        external_loads = []
+        ext_load = []
 
         for step_i in range(self.n_intervals):
             # print('step {}: {}'.format(step_i, current_time))
             res = strat.step(event_steps[step_i])
             gcs = strat.world_state.grid_connectors.values()
             # get external loads (all current loads without charging stations)
-            external_loads.append(sum([gc.get_external_load(self.constants.charging_stations.keys()) for gc in gcs]))
+            ext_load.append(sum([gc.get_external_load(self.constants.charging_stations.keys()) for gc in gcs]))
             results.append(res)
 
-            # get costs
+            # get prices and costs
             cost = 0
-            for gc in gcs:
+            price = []
+            for gc_id, gc in sorted(strat.world_state.grid_connectors.items()):
                 cost += util.get_cost(sum(gc.current_loads.values()), gc.cost)
-                costs.append(cost)
+                price.append(util.get_cost(1, gc.cost))
+            costs.append(cost)
+            prices.append(price)
 
         print("Costs:", int(sum(costs)))
 
         if options.get('visual', False):
             print('Done. Create plots...')
-            charging_stations = {}
-            socs = {}
-            sum_cs = {}
 
-            # find all charging stations and vehicles in results
+            socs  = []
+            sum_cs = []
+            xlabels = []
+
             for r in results:
-                for cs_id in r['commands'].keys():
-                    if cs_id not in charging_stations:
-                        charging_stations[cs_id] = {'x': [], 'y': []}
-                for v_id in r['socs'].keys():
-                    if v_id not in socs:
-                        socs[v_id] = {'x': [], 'y': []}
-                sum_cs[r['current_time']] = 0.0
+                xlabels.append(r['current_time'])
 
-            # find in result or NULL value for each timestep
-            for r in results:
-                time = r['current_time']
-                for cs_id in charging_stations.keys():
-                    charging_stations[cs_id]['x'].append(time)
-                    if cs_id in r['commands']:
-                        charging_stations[cs_id]['y'].append(r['commands'][cs_id])
-                        sum_cs[time] += r['commands'][cs_id]
-                    else:
-                        charging_stations[cs_id]['y'].append(0.0)
-
-                for vehicle_id, soc in r['socs'].items():
-                    for vehicle_id in socs.keys():
-                        socs[vehicle_id]['x'].append(time)
-                        if vehicle_id in r['socs']:
-                            socs[vehicle_id]['y'].append(r['socs'][vehicle_id])
-                        else:
-                            socs[vehicle_id]['y'].append(math.nan)
+                cur_car = []
+                cur_cs  = []
+                for v_id in sorted(self.constants.vehicles):
+                    cur_car.append(r['socs'].get(v_id, None))
+                socs.append(cur_car)
+                for cs_id in sorted(self.constants.charging_stations):
+                    cur_cs.append(r['commands'].get(cs_id, 0.0))
+                sum_cs.append(cur_cs)
 
             # plot!
+            fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
+            fig.suptitle('Strategy: {}'.format(strat.description), fontweight='bold')
 
-            fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-            # plt.xticks
-            fig.suptitle('Strategy: {}'.format(strategy_name), fontweight='bold')
-
-            for name, values in sorted(charging_stations.items()):
-                _, = ax1.step(values['x'], values['y'], label=name)
+            lines = ax1.step(xlabels, sum_cs)
             ax1.set_title('Charging Stations')
             ax1.set(ylabel='Power in kW')
-            if len(charging_stations) <= 10:
-                ax1.legend()
+            if len(self.constants.charging_stations) <= 10:
+                ax1.legend(lines, sorted(self.constants.charging_stations.keys()))
 
-            for name, values in sorted(socs.items()):
-                _, = ax2.step(values['x'], values['y'], label=name)
+            lines = ax2.step(xlabels, socs)
             ax2.set_title('Vehicles')
             ax2.set(ylabel='SOC in %')
-            if len(charging_stations) <= 10:
-                ax2.legend()
+            if len(self.constants.vehicles) <= 10:
+                ax2.legend(lines, sorted(self.constants.vehicles.keys()))
 
-            ax3.plot(list(sum_cs.keys()), list(sum_cs.values()), label="CS")
-            ax3.plot(list(sum_cs.keys()), external_loads, label="external")
-            total_power = list([external_loads[i] + cs_power for i, cs_power in enumerate(sum_cs.values())])
-            ax3.plot(list(sum_cs.keys()), total_power, label="total")
+            ax3.plot(xlabels, list([sum(cs) for cs in sum_cs]), label="CS")
+            ax3.plot(xlabels, ext_load, label="external")
+            total_power = list([ext_load[i] + sum(sum_cs[i]) for i in range(len(xlabels))])
+            ax3.plot(xlabels, total_power, label="total")
             ax3.set_title('Cumulative Power')
             ax3.set(ylabel='Power in kW')
             ax3.legend()
             ax3.xaxis_date() # xaxis are datetime objects
-            # fig.autofmt_xdate() # rotate xaxis labels (dates) to fit
 
+            lines = ax4.step(xlabels, prices)
+            ax4.set_title('Price for 1 kWh')
+            ax4.set(ylabel='€')
+            if len(self.constants.grid_connectors) <= 10:
+                ax4.legend(lines, sorted(self.constants.grid_connectors.keys()))
+
+            fig.autofmt_xdate() # rotate xaxis labels (dates) to fit
             plt.show()
-
 
 if __name__ == '__main__':
 
