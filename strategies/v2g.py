4d9f272dd7326054b99abf8b608450315268aa41
stefan.schirmeister <stefan.schirmeister@localiser.de>
2020-11-25 17:07:36 +0100

    vsg: better simulation, better distribution

--- strategies/v2g.py
+++ strategies/v2g.py
@@ -18,10 +18,11 @@ class V2g(Strategy):
         self.LPF = 0.75
         self.LOAD_STRAT = 'needy' # greedy, needy, balanced
         self.SAFE_DISCHARGE = 0
+        self.USE_COST = 0
 
         # init parent class Strategy. May override defaults
         super().__init__(constants, start_time, **kwargs)
-        self.description = "v2g_{}".format(self.LOAD_STRAT)
+        self.description = "v2g ({})".format(self.LOAD_STRAT)
 
         # limitations: one grid connector, linear costs
         assert len(constants.grid_connectors) == 1, "Only one grid connector supported"
@@ -71,10 +72,12 @@ class V2g(Strategy):
         hours_per_timestep=self.interval.total_seconds() / 3600.0
         timesteps = []
         gc_info = {
-            'max_power': gc.max_power,
+            'max_power': gc.cur_max_power,
             'cost': gc.cost,
             'load': gc.get_external_load()
         }
+        min_cost = util.get_cost(0, gc.cost)
+        max_cost = util.get_cost(gc.cur_max_power, gc.cost)
 
         cur_time = self.current_time - self.interval
         for step_idx in range(timesteps_per_day):
@@ -107,7 +110,7 @@ class V2g(Strategy):
                     # update GC info
                     max_power = event.max_power or gc_info['max_power']
                     gc_info['max_power'] = min(gc_info['max_power'], max_power)
-                    gc_info['cost']= event.cost
+                    gc_info['cost'] = event.cost
 
             # predicted external load
             if step_idx > 0:
@@ -115,7 +118,8 @@ class V2g(Strategy):
                 weekday_idx  = cur_time.weekday()
                 interval_idx = int((cur_time - midnight) / self.interval)
                 gc_info['load'] = self.pred_ext_load[weekday_idx][interval_idx]
-                # print(step_idx, weekday_idx, interval_idx, gc_info['load'])
+                min_cost = min(min_cost, util.get_cost(0, gc_info['cost']))
+                max_cost = max(max_cost, util.get_cost(gc_info['max_power'], gc_info['cost']))
 
             timesteps.append(gc_info.copy())
 
@@ -124,69 +128,186 @@ class V2g(Strategy):
             socs={vid: v.battery.soc for vid, v in self.world_state.vehicles.items()}
             return {'current_time': self.current_time, 'commands': {}, 'socs': socs}
 
-        min_power = 0
-        max_power = max([info['max_power'] for info in timesteps])
+        if self.USE_COST == 0:
+            min_power = 0
+            max_power = max([info['max_power'] for info in timesteps])
 
-        while max_power - min_power > self.EPS:
-            cur_power = (max_power + min_power) / 2
-            energy_loaded = 0
-            energy_discharged = 0
-            for ts_idx, gc_info in enumerate(timesteps):
-                usable_power = min(cur_power, gc_info['max_power'])
-                usable_power -= gc_info['load']
-                if usable_power > 0:
-                    # load vehicles
-                    energy_loaded += usable_power * hours_per_timestep
+            while max_power - min_power > self.EPS:
+                cur_power = (max_power + min_power) / 2
+                energy_loaded = 0
+                energy_discharged = 0
+                for ts_idx, gc_info in enumerate(timesteps):
+                    usable_power = min(cur_power, gc_info['max_power'])
+                    usable_power -= gc_info['load']
+                    if usable_power > 0:
+                        # load vehicles
+                        energy_loaded += usable_power * hours_per_timestep
+                    else:
+                        # use vehicles to support external load
+                        energy_discharged -= usable_power * hours_per_timestep
+
+                if energy_loaded < energy_needed:
+                    # fail: not enough charged
+                    min_power = cur_power
+                elif energy_loaded + energy_avail > energy_discharged:
+                    # loaded too much
+                    max_power = cur_power
                 else:
-                    # use vehicles to support external load
-                    energy_discharged -= usable_power * hours_per_timestep
-            # print(min_power, cur_power, max_power)
+                    # success: vehicles charged, try to build buffer
+                    min_power = cur_power
 
-            if energy_loaded < energy_needed:
-                # fail: not enough charged
-                min_power = cur_power
-            elif energy_loaded < energy_discharged:
-                # success, but external load not saturated
-                min_power = cur_power
-            else:
-                # success: vehicles charged and external load saturated
-                max_power = cur_power
+            usable_power = min(gc.cur_max_power, cur_power) - gc.get_external_load()
+        elif util.get_cost(gc.cur_max_power, gc.cost) < 0:
+            # negative price: full power
+            usable_power = gc.cur_max_power - gc.get_external_load()
+        elif self.USE_COST == 1:
+            min_power = 0
+            max_power = max([info['max_power'] for info in timesteps])
+            last_cost = None
+            increase = None
+            while max_power - min_power > self.EPS:
+                cur_power = (max_power + min_power) / 2
+                energy_loaded = 0
+                energy_discharged = 0
+                cost = 0
+                for ts_idx, gc_info in enumerate(timesteps):
+                    total_power = min(cur_power, gc_info['max_power'])
+                    # use saved energy to reduce external load
+                    usable_power = total_power - gc_info['load']
+                    cost += util.get_cost(usable_power, gc_info['cost'])
+                    if usable_power > 0:
+                        # load vehicles
+                        energy_loaded += usable_power * hours_per_timestep
+                    else:
+                        # use vehicles to support external load
+                        energy_discharged -= usable_power * hours_per_timestep
+
+                if energy_loaded < energy_needed:
+                    # fail: not enough charged
+                    min_power = cur_power
+                    increase = True
+                elif increase is None:
+                    # first iteration: check direction
+                    if last_cost is None:
+                        # check higher power (easier to reset)
+                        min_power = cur_power
+                        # don't set increase yet
+                    else:
+                        # second iteration: check success of higher power
+                        if cost <= last_cost:
+                            # success! Higher power is cheaper
+                            # continue raising power
+                            min_power = cur_power
+                            increase = True
+                        else:
+                            # oops, higher power is more expensive
+                            # reset last iteration
+                            min_power = 0
+                            # check lower cost
+                            max_power = cur_power
+                            increase = False
+                else:
+                    # later iterations: check cost change
+                    if cost < last_cost:
+                        # cheaper: continue same way
+                        if increase:
+                            min_power = cur_power
+                        else:
+                            max_power = cur_power
+                    elif cost == last_cost:
+                        # no change: try higher power
+                        min_power = cur_power
+                        increase = True
+                    else:
+                        # more expensive: go other way
+                        if increase:
+                            max_power = cur_power
+                        else:
+                            min_power = cur_power
+                        increase = not increase
+                last_cost = cost
+
+            usable_power = min(gc.cur_max_power, cur_power) - gc.get_external_load()
+        else:
+            # get power by price
+            while max_cost - min_cost > self.EPS:
+                cur_cost = (max_cost + min_cost) / 2
+                energy_loaded = 0
+                energy_discharged = 0
+
+                for ts_idx, gc_info in enumerate(timesteps):
+                    total_power = util.get_power(cur_cost, gc_info['cost'])
+                    total_power = min(total_power, gc_info['max_power'])
+                    usable_power = total_power - gc_info['load']
+                    if usable_power > 0:
+                        # load vehicles
+                        energy_loaded += usable_power * hours_per_timestep
+                    else:
+                        # use vehicles to support external load
+                        energy_discharged -= usable_power * hours_per_timestep
+
+                if energy_loaded < energy_needed:
+                    # fail: not enough charged
+                    min_cost = cur_cost
+                elif energy_loaded + energy_avail > energy_discharged:
+                    # success, but surplus energy
+                    max_cost = cur_cost
+                else:
+                    min_cost = cur_cost
+            usable_power = util.get_power(cur_cost, gc.cost)
+            usable_power = min(usable_power, gc.cur_max_power)
+            usable_power -= gc.get_external_load()
 
         charging_stations = {}
-        usable_power = min(gc.max_power, cur_power) - gc.get_external_load()
         vehicle_list = sorted(vehicles.values(), key=self.sort_key)
         if usable_power > 0:
             # load vehicles
+            # prioritize vehicles below desired SOC
+            vehicles = list(filter(lambda v: v.get_delta_soc() > 0, vehicle_list))
+            used_power = 0
+            for vehicle in vehicles:
+                if self.LOAD_STRAT == 'greedy':
+                    # charge one vehicle after the other
+                    p = max(usable_power - used_power, 0)
+                    load = vehicle.battery.load(self.interval, p)
+                elif self.LOAD_STRAT == 'needy' and energy_needed > 0:
+                    f = vehicle.get_delta_soc()/100 * vehicle.battery.capacity / energy_needed
+                    load = vehicle.battery.load(self.interval, usable_power * f)
+                elif self.LOAD_STRAT == 'balanced':
+                    # distribute among vehicles in need
+                    load = vehicle.battery.load(self.interval, usable_power / len(vehicles))
+
+                cs_id = vehicle.connected_charging_station
+                cs = self.world_state.charging_stations[cs_id]
+                avg_power = load["avg_power"]
+                charging_stations[cs_id] = gc.add_load(cs_id, avg_power)
+                cs.current_power += avg_power
+                used_power += avg_power
+                max_energy_needed -= load["soc_delta"]/100 * vehicle.battery.capacity
+                if gc.cur_max_power != gc.max_power:
+                    print(avg_power, max_energy_needed)
+
+            # distribute surplus
+            usable_power -= used_power
             for vehicle in vehicle_list:
                 if usable_power <= 0:
                     break
                 if self.LOAD_STRAT == 'greedy':
                     # charge one vehicle after the other
-                    avg_power = vehicle.battery.load(self.interval, usable_power)['avg_power']
-                    usable_power -= avg_power
+                    avg_power = vehicle.battery.load(self.interval, usable_power)["avg_power"]
                 elif self.LOAD_STRAT == 'needy' and max_energy_needed > 0:
-                    # delta_soc = max(vehicle.get_delta_soc(), 0)
-                    # delta_soc = 100 - vehicle.battery.soc
-                    # delta_energy = (delta_soc / 100) * vehicle.battery.capacity
-                    # f = delta_energy / max_energy_needed
-                    # f = delta_energy / energy_needed
-                    delta_soc = vehicle.get_delta_soc() / 100
-                    if delta_soc > 0:
-                        f = delta_soc * vehicle.battery.capacity / energy_needed
-                        max_energy_needed - energy_needed
-                    else:
-                        f = -delta_soc * vehicle.battery.capacity / max_energy_needed
-                    avg_power = vehicle.battery.load(self.interval, usable_power * f)['avg_power']
+                    delta_soc = 1 - vehicle.battery.soc/100
+                    f = delta_soc * vehicle.battery.capacity / max_energy_needed
+                    avg_power = vehicle.battery.load(self.interval, usable_power * f)["avg_power"]
                 elif self.LOAD_STRAT == 'balanced':
-                    # distribute among remaining vehicles
-                    avg_power = vehicle.battery.load(self.interval, usable_power / len(vehicle_list))['avg_power']
-                else:
-                    continue
+                    # distribute among vehicles
+                    avg_power = vehicle.battery.load(self.interval, usable_power / len(vehicle_list))["avg_power"]
 
                 cs_id = vehicle.connected_charging_station
                 cs = self.world_state.charging_stations[cs_id]
                 charging_stations[cs_id] = gc.add_load(cs_id, avg_power)
                 cs.current_power += avg_power
+
         else:
             # discharge vehicles
             vehicle_list.reverse()
@@ -205,15 +326,12 @@ class V2g(Strategy):
                     usable_power -= avg_power
                 elif self.LOAD_STRAT == 'needy' and energy_avail > 0:
                     # discharge in relation to available energy
-                    delta_soc = -vehicle.get_delta_soc()
-                    delta_energy = (delta_soc / 100) * vehicle.battery.capacity
-                    f = delta_energy / energy_avail
+                    delta_soc = -vehicle.get_delta_soc() / 100
+                    f = delta_soc * vehicle.battery.capacity / energy_avail
                     avg_power = vehicle.battery.unload(self.interval, usable_power * f, vehicle.desired_soc)['avg_power']
                 elif self.LOAD_STRAT == 'balanced':
                     # discharge evenly
                     avg_power = vehicle.battery.unload(self.interval, usable_power / len(vehicle_list), vehicle.desired_soc)['avg_power']
-                else:
-                    continue
 
                 cs_id = vehicle.connected_charging_station
                 cs = self.world_state.charging_stations[cs_id]
