7a3c95a32249e53bc6a5d3e1b0eea92ae4b0f653
stefan.schirmeister <stefan.schirmeister@localiser.de>
2021-03-22 16:49:57 +0100

    enable stratagy names with underscore

--- netz_elog/strategy.py
+++ netz_elog/strategy.py
@@ -5,9 +5,10 @@ from netz_elog import events, util
 
 
 def class_from_str(strategy_name):
-    strategy_name = strategy_name.lower()
-    module = import_module('netz_elog.strategies.' + strategy_name)
-    return getattr(module, strategy_name.capitalize())
+    import_name = strategy_name.lower()
+    class_name = "".join([s.capitalize() for s in strategy_name.split('_')])
+    module = import_module('netz_elog.strategies.' + import_name)
+    return getattr(module, class_name)
 
 
 class Strategy():
