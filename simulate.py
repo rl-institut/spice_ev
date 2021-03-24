940389a16d5be63c2b7235eb31afcd47eb5ca9b8
Johannes Hofmann <mail@b-r-u.org>
2021-03-24 09:02:11 +0100

    simulate: Add --strategy-option
    to avoid command line parameters with an unspecified number of
    arguments, which sometimes cause ambiguity with different orderings of
    the parameters.

--- simulate.py
+++ simulate.py
@@ -23,8 +23,10 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Netz_eLOG modelling tool. Simulate different charging strategies for a given scenario.')
     parser.add_argument('file', help='Set the scenario JSON file')
-    parser.add_argument('--strategy', '-s', nargs='+', default=['greedy'], help='Specify the charging strategy. '
-        'One of {}. You may define custom options in the form option=value.'.format(', '.join(strategies)))
+    parser.add_argument('--strategy', '-s', default='greedy', help='Specify the charging strategy. '
+        'One of {}. You may define custom options with --strategy-option.'.format(', '.join(strategies)))
+    parser.add_argument('--strategy-option', '-so', metavar=('KEY', 'VALUE'), nargs=2, action='append',
+        help='Append additional options to the charging strategy.')
     parser.add_argument('--visual', '-v', action='store_true', help='Show plots of the results')
     args = parser.parse_args()
 
@@ -33,22 +35,18 @@ if __name__ == '__main__':
     # parse strategy options
     if args.strategy:
         # first argument: strategy name
-        strategy_name = args.strategy.pop(0)
+        strategy_name = args.strategy
         if strategy_name not in strategies:
             raise NotImplementedError("Unknown strategy: {}".format(strategy_name))
-        for opt_string in args.strategy:
-            try:
-                # key=value
-                opt_key, opt_val = opt_string.split('=')
-            except ValueError:
-                print("Ignored option {}. Need options in the form key=value".format(opt_string))
-            try:
-                # option may be number
-                opt_val = float(opt_val)
-            except ValueError:
-                # or not
-                pass
-            options[opt_key] = opt_val
+        if args.strategy_option:
+            for opt_key, opt_val in args.strategy_option:
+                try:
+                    # option may be number
+                    opt_val = float(opt_val)
+                except ValueError:
+                    # or not
+                    pass
+                options[opt_key] = opt_val
 
     # Read JSON
     with open(args.file, 'r') as f:
