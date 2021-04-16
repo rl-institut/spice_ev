b8c6997ec93f9d96393e1da3d07428f010c110f6
stefan.schirmeister <stefan.schirmeister@localiser.de>
2021-04-16 16:25:37 +0200

    reworked generate_from_simbev
    - random price generation, can be seeded or made constant
    - different charging curves for vehicle types
    - made ready to support different use cases
    - rewritten vehicle and CS creation logic
    - get number of intervals from input files (end of last charging event)

--- generate_from_simbev.py
+++ generate_from_simbev.py
@@ -3,6 +3,7 @@
 import argparse
 import csv
 import datetime
+from functools import reduce
 import json
 import math
 from pathlib import Path
@@ -16,8 +17,8 @@ if __name__ == '__main__':
     parser.add_argument('output', help='output file name (example.json)')
     parser.add_argument('--simbev', metavar='DIR', type=str, required=True, help='set directory with SimBEV files')
     parser.add_argument('--interval', metavar='MIN', type=int, default=15, help='set number of minutes for each timestep (Δt)')
-    parser.add_argument('--days', metavar='N', type=int, default=7, help='set duration of simulation')
-    parser.add_argument('--desired-soc', metavar='SOC', type=int, default=80, help='set desired SOC (0%% - 100%%) for each charging process')
+    # parser.add_argument('--use-case', '-uc', default='home,work', help='SimBEV charging use case. Default: home,work (charge at home and at work)')
+    parser.add_argument('--price-seed', '-s', type=int, default=0, help='set seed when generating energy market prices. Negative values for fixed price in cents')
     #parser.add_argument('--include_csv', nargs='*', help='include CSV for external load. You may define custom options in the form option=value')
     #parser.add_argument('--external_csv', nargs='?', help='generate CSV for external load. Not implemented.')
     args = parser.parse_args()
@@ -25,8 +26,10 @@ if __name__ == '__main__':
     # first monday of 2021
     # SimBEV uses MiD data and creates data for an exemplary week, so there are no exact dates.
     start = datetime.datetime(year=2021, month=1, day=4, tzinfo=datetime.timezone(datetime.timedelta(hours=1)))
-    stop = start + datetime.timedelta(days=args.days)
     interval = datetime.timedelta(minutes=args.interval)
+    n_intervals = 0
+    price_stable_hours = 6 # for random price: remains stable for X hours
+    price_interval = datetime.timedelta(hours=price_stable_hours) / interval # every X timesteps, generate new price signal
 
     # possible vehicle types
     vehicle_types = {
@@ -34,21 +37,21 @@ if __name__ == '__main__':
             "name": "bev_luxury",
             "capacity": 120, # kWh
             "mileage": 40, # kWh / 100km
-            "charging_curve": [[0, 22], [80, 22], [100, 0]], # SOC -> kWh
+            "charging_curve": [[0, 300], [80, 300], [100, 300]], # SOC -> kWh
             "min_charging_power": 0,
         },
         "bev_medium": {
             "name": "bev_medium",
             "capacity": 100, # kWh
             "mileage": 40, # kWh / 100km
-            "charging_curve": [[0, 22], [80, 22], [100, 0]], # SOC -> kWh
+            "charging_curve": [[0, 150], [80, 150], [100, 150]], # SOC -> kWh
             "min_charging_power": 0,
         },
         "bev_mini": {
             "name": "bev_mini",
             "capacity": 70, # kWh
             "mileage": 40, # kWh / 100km
-            "charging_curve": [[0, 22], [80, 22], [100, 0]], # SOC -> kWh
+            "charging_curve": [[0, 50], [80, 50], [100, 50]], # SOC -> kWh
             "min_charging_power": 0,
         },
         "phev_luxury": {
@@ -74,110 +77,151 @@ if __name__ == '__main__':
         },
     }
 
+    """
     # Infer vehicle type from file name
     def vehicle_type_from_path(path):
+        print(path, path.stem)
         for t in vehicle_types.keys():
             if t in str(path.stem).lower():
                 return t
-
-    # Create vehicle name from file name
-    def vehicle_name_from_path(path):
-        return str(path.stem)[:-4]
-
+        raise NotImplementedError("Unknown vehicle type: {}".format(path.stem))
+    """
 
     def datetime_from_timestep(timestep):
         assert type(timestep) == int
         return start + (interval * timestep)
 
+    # use_cases = args.use_cases.split(',_')
 
     # CSV files
     pathlist = list(Path(args.simbev).rglob('*.csv'))
     pathlist.sort()
 
-    # VEHICLES WITH THEIR CHARGING STATION
-    # create vehicles and their charging stations
     vehicles = {}
     charging_stations = {}
-    for csv_path in pathlist:
-        v_type = vehicle_type_from_path(csv_path)
-        v_name = vehicle_name_from_path(csv_path)
-        cs_name = "CS_" + v_name
-        soc = 100
-        vehicles[v_name] = {
-            "connected_charging_station": None,
-            "soc": soc,
-            "vehicle_type": v_type
-        }
-
-        charging_stations[cs_name] = {
-            "max_power": max([v[1] for v in vehicle_types[v_type]['charging_curve']]),
-            "parent": "GC1"
-        }
-
     events = {
-        "grid_operator_signals": [
-            {
-                "signal_time": start.isoformat(),
-                "grid_connector_id": "GC1",
-                "start_time": start.isoformat(),
-                "cost": {
-                    "type": "polynomial",
-                    "value": [0.0, 0.32, 0.0]
-                }
-            },
-        ],
+        "grid_operator_signals": [],
         "external_load": {},
         "vehicle_events": []
     }
 
+    if args.price_seed < 0:
+        # use fixed price
+        events["grid_operator_signals"].append({
+            "signal_time": start.isoformat(),
+            "grid_connector_id": "GC1",
+            "start_time": start.isoformat(),
+            "cost": {
+                "type": "fixed",
+                "value": -args.price_seed/100
+            }
+        })
+    else:
+        random.seed(args.price_seed)
+
     for csv_path in pathlist:
+        vehicle_name = str(csv_path.stem)[:-4]
         with open(csv_path, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
-            for row in reader:
-                if row['location'] not in ['0_work', '6_home']:
-                    continue
-
-                charging_demand = float(row['chargingdemand'])
-                if charging_demand <= 0.0:
-                    continue
-
-                v_name = vehicle_name_from_path(csv_path)
-                v_type = vehicle_type_from_path(csv_path)
-                cs_name = "CS_" + v_name
-
-                event_start = datetime_from_timestep(int(row['charge_start']))
-                event_end = datetime_from_timestep(int(row['charge_end']))
-
-                soc_delta = -100.0 * charging_demand / vehicle_types[v_type]['capacity']
-
-                events["vehicle_events"].append({
-                    "signal_time": event_start.isoformat(),
-                    "start_time": event_start.isoformat(),
-                    "vehicle_id": v_name,
-                    "event_type": "arrival",
-                    "update": {
-                        "connected_charging_station": cs_name,
-                        "estimated_time_of_departure": event_end.isoformat(),
-                        "desired_soc": args.desired_soc,
-                        "soc_delta": soc_delta
+            for idx,row in enumerate(reader):
+                if vehicle_name not in vehicles:
+                    # set vehicle info from first data row
+                    v_type = row["car_type"]
+                    assert v_type in vehicle_types, "Unknown vehicle type for {}: {}".format(vehicle_name, v_type)
+                    vehicles[vehicle_name] = {
+                        "connected_charging_station": None,
+                        "soc": float(row["SoC_end"]) * 100,
+                        "vehicle_type": v_type
                     }
-                })
 
-                events["vehicle_events"].append({
-                    "signal_time": event_end.isoformat(),
-                    "start_time": event_end.isoformat(),
-                    "vehicle_id": v_name,
-                    "event_type": "departure",
-                    "update": {
-                        "estimated_time_of_arrival": None
-                    }
-                })
+                # charge at this location?
+                location = row["location"]
+                capacity = float(row["netto_charging_capacity"])
+                demand   = float(row["chargingdemand"])
+                charge_here = (
+                    capacity > 0
+                    and demand > 0
+                    # and reduce((lambda b, uc: b or (uc in location)), use_cases)
+                )
+                if charge_here:
+                    # set up charging point at location
+                    cs_name = "{}_{}".format(vehicle_name, location.split('_')[-1])
+                    if cs_name in charging_stations and charging_stations[cs_name]["max_power"] != capacity:
+                        # same location type, different capacity: build new CS
+                        cs_name = "{}_{}".format(cs_name, idx)
+                    if cs_name not in charging_stations:
+                        charging_stations[cs_name] = {
+                            # get max power from charging curve
+                            "max_power": capacity,
+                            "parent": "GC1"
+                        }
+
+                    # create vehicle events
+                    park_start = int(row["park_start"])
+                    park_end = int(row["park_end"])
+                    event_start = datetime_from_timestep(int(row['park_start']))
+                    event_end = datetime_from_timestep(int(row['park_end']))
+                    soc_delta = -100.0 * demand / vehicle_types[v_type]['capacity']
+
+                    events["vehicle_events"].append({
+                        "signal_time": event_start.isoformat(),
+                        "start_time": event_start.isoformat(),
+                        "vehicle_id": vehicle_name,
+                        "event_type": "arrival",
+                        "update": {
+                            "connected_charging_station": cs_name,
+                            "estimated_time_of_departure": event_end.isoformat(),
+                            "desired_soc": float(row["SoC_end"]) * 100,
+                            "soc_delta": soc_delta
+                        }
+                    })
+
+                    events["vehicle_events"].append({
+                        "signal_time": event_end.isoformat(),
+                        "start_time": event_end.isoformat(),
+                        "vehicle_id": vehicle_name,
+                        "event_type": "departure",
+                        "update": {
+                            "estimated_time_of_arrival": None
+                        }
+                    })
+
+                    n_intervals = max(n_intervals, park_end)
+
+                    # random price: each price interval, generate new price
+
+                    while args.price_seed >= 0 and n_intervals >= price_interval * len(events["grid_operator_signals"]):
+                        price_update_idx = int(len(events["grid_operator_signals"]) * price_interval)
+                        start_time = datetime_from_timestep(price_update_idx)
+                        signal_time = max(start, start_time - datetime.timedelta(days=1))
+                        if 6 < start_time.hour < 18:
+                            # daytime: ~15ct
+                            events['grid_operator_signals'].append({
+                                "signal_time": signal_time.isoformat(),
+                                "grid_connector_id": "GC1",
+                                "start_time": start_time.isoformat(),
+                                "cost": {
+                                    "type": "fixed",
+                                    "value": 0.15 + random.gauss(0, 0.05)
+                                }
+                            })
+                        else:
+                            # nighttime: ~5ct
+                            events['grid_operator_signals'].append({
+                                "signal_time": signal_time.isoformat(),
+                                "grid_connector_id": "GC1",
+                                "start_time": start_time.isoformat(),
+                                "cost": {
+                                    "type": "fixed",
+                                    "value": 0.15 + random.gauss(0, 0.05)
+                                }
+                            })
 
     j = {
         "scenario": {
             "start_time": start.isoformat(),
             "interval": args.interval,
-            "n_intervals": math.ceil((stop - start) / interval),
+            "n_intervals": n_intervals,
         },
         "constants": {
             "vehicle_types": vehicle_types,
