2e02b147de68a558bbbaba0c99f4f03357b806aa
stefan.schirmeister <stefan.schirmeister@localiser.de>
2021-08-25 17:53:53 +0200

    improve peak_load_window strategy

--- src/strategies/peak_load_window.py
+++ src/strategies/peak_load_window.py
@@ -27,7 +27,7 @@ class PeakLoadWindow(Strategy):
         elif self.LOAD_STRAT == 'needy':
             self.sort_key = lambda v: (-v.get_energy_needed())
         elif self.LOAD_STRAT == 'balanced':
-            self.sort_key = lambda v: v.connected_charging_station  # order does not matter
+            self.sort_key = lambda v: -v.get_energy_needed(full=False)
         else:
             raise NotImplementedError(self.LOAD_STRAT)
 
@@ -58,6 +58,9 @@ class PeakLoadWindow(Strategy):
     def step(self, event_list=[]):
         super().step(event_list)
 
+        timesteps_per_day = datetime.timedelta(days=1) // self.interval
+        timesteps_per_hour = datetime.timedelta(hours=1) / self.interval
+
         gc = list(self.world_state.grid_connectors.values())[0]
 
         ts = [{
@@ -71,17 +74,23 @@ class PeakLoadWindow(Strategy):
         energy_needed = 0
         for vid, v in self.world_state.vehicles.items():
             if v.connected_charging_station is not None:
+                assert vid in v.connected_charging_station
                 vehicles[vid] = v
-                energy_needed += v.get_energy_needed(full=True)
+                energy_needed += v.get_energy_needed(full=False)
                 standing[vid] = 0
+                if v.estimated_time_of_departure is not None:
+                    # how many timesteps left until leaving?
+                    v.dep_ts = -(self.current_time - v.estimated_time_of_departure) // self.interval
+                else:
+                    # no departure time given: assume whole day
+                    v.dep_ts = timesteps_per_day
         sum_loads = sum(gc.current_loads.values())
 
         # peek into future events for external loads, feed-in and schedule
         event_idx = 0
         cur_time = self.current_time - self.interval
         # look one day ahead
-        timesteps_per_day = datetime.timedelta(days=1) // self.interval
-        timesteps_per_hour = datetime.timedelta(hours=1) / self.interval
+
         for timestep_idx in range(timesteps_per_day):
             cur_time += self.interval
 
@@ -112,33 +121,39 @@ class PeakLoadWindow(Strategy):
             # end of useful events
 
         safe = False
+        target = 0
         max_target = gc.max_power
         min_target = -gc.max_power
         sim_vehicles = deepcopy(vehicles)
         sim_batteries = deepcopy(self.world_state.batteries)
-        while not safe and (max_target - min_target) > self.EPS:
+        outside_windows = [not info["window"] for info in ts]
+
+        while not safe or (target and (max_target - min_target) > self.EPS):
             target = (min_target + max_target) / 2
             cur_time = self.current_time - self.interval
 
             # reset SoC
             for vid, v in sim_vehicles.items():
                 v.battery.soc = vehicles[vid].battery.soc
+                assert vid in v.connected_charging_station
             for bid, b in sim_batteries.items():
                 b.soc = self.world_state.batteries[bid].soc
 
-            for ts_info in ts:
+            # simulate future timesteps
+            for ts_idx, ts_info in enumerate(ts):
                 cur_time += self.interval
                 cur_loads = sum(ts_info["loads"].values())
                 safe = True
                 sim_charging_vehicles = []
                 sim_energy_needed = 0
+                # check if desired SoC is met when leaving or take note of energy needed
                 for v in sim_vehicles.values():
                     if (v.estimated_time_of_departure is not None
                             and v.estimated_time_of_departure <= cur_time):
                         safe &= v.get_delta_soc() < self.EPS
                     else:
                         sim_charging_vehicles.append(v)
-                        sim_energy_needed += v.get_energy_needed(full=True)
+                        sim_energy_needed += v.get_energy_needed(full=False)
 
                 if not safe:
                     # at least one vehicle not sufficiently charged
@@ -159,12 +174,13 @@ class PeakLoadWindow(Strategy):
                     # outside of window: draw power
                     power = max(gc.max_power - cur_loads, 0)
                     if self.LOAD_STRAT == "balanced":
+                        # special substrategy: distribute energy over standing period
                         for v in sim_charging_vehicles:
-                            remain_ts = (v.estimated_time_of_departure - cur_time) / self.interval
+                            num_outside_ts = sum(outside_windows[ts_idx:v.dep_ts])
                             # get power needed to reach desired SoC
                             v_power = v.get_energy_needed(full=False) * timesteps_per_hour
                             # distribute needed power over remaining timesteps
-                            v_power /= remain_ts
+                            v_power /= num_outside_ts
                             # scale with battery efficiency
                             v_power /= v.battery.efficiency
                             cs_id = vehicle.connected_charging_station
@@ -172,8 +188,8 @@ class PeakLoadWindow(Strategy):
                             # clamp power
                             v_power = util.clamp_power(v_power, vehicle, cs)
                             # charging
-                            v_power = v.battery.load(self.interval, v_power)["avg_power"]
-                            power -= v_power
+                            info = v.battery.load(self.interval, v_power, v.desired_soc)
+                            power -= info["avg_power"]
                     else:
                         # default: distribute power according to LOAD_STRAT
                         info = self.distribute_power(
@@ -208,30 +224,32 @@ class PeakLoadWindow(Strategy):
             power = max(gc.max_power - sum_loads, 0)
             if self.LOAD_STRAT == "balanced":
                 commands = {}
-                for v in vehicles.values():
-                    # estimate remaining standing time
-                    remain_ts = (v.estimated_time_of_departure - self.current_time) / self.interval
+                for vid, v in vehicles.items():
+                    num_outside_ts = sum(outside_windows[:v.dep_ts])
                     # get power needed to reach desired SoC
                     v_power = v.get_energy_needed(full=False) * timesteps_per_hour
                     # distribute power over estimated remaining standing time
-                    v_power /= remain_ts
+                    v_power /= num_outside_ts
                     # scale with battery efficiency
                     v_power /= v.battery.efficiency
-                    cs_id = vehicle.connected_charging_station
+                    cs_id = v.connected_charging_station
                     cs = self.world_state.charging_stations[cs_id]
-                    v_power = util.clamp_power(v_power, vehicle, cs)
+                    v_power = util.clamp_power(v_power, v, cs)
                     # charging
-                    v_power = v.battery.load(self.interval, v_power)["avg_power"]
+                    v_power = v.battery.load(self.interval, v_power, v.desired_soc)["avg_power"]
                     gc.add_load(cs_id, v_power)
                     commands[cs_id] = v_power
                     power -= v_power
             else:
+                # default: charge as per LOAD_STRAT
                 commands = self.distribute_power(vehicles.values(), power, energy_needed)
                 power -= sum(commands.values())
+            # charge batteries
             for bid, battery in self.world_state.batteries.items():
                 bat_power = battery.load(self.interval, power)["avg_power"]
                 power -= bat_power
                 gc.add_load(bid, bat_power)
+        # update GC loads
         for cs_id, avg_power in commands.items():
             gc.add_load(cs_id, avg_power)
 
@@ -257,18 +275,19 @@ class PeakLoadWindow(Strategy):
                 power = available_power
             elif self.LOAD_STRAT == "needy":
                 # get fraction of precalculated power need to overall power need
-                vehicle_energy_needed = vehicle.get_energy_needed(full=True)
+                vehicle_energy_needed = vehicle.get_energy_needed(full=False)
                 frac = vehicle_energy_needed / energy_needed if energy_needed > self.EPS else 0
                 power = available_power * frac
                 energy_needed -= vehicle_energy_needed
             elif self.LOAD_STRAT == "balanced":
+                # distribute total power over vehicles
                 power = available_power / vehicles_to_charge
 
             cs_id = vehicle.connected_charging_station
             cs = self.world_state.charging_stations[cs_id]
             power = util.clamp_power(power, vehicle, cs)
             # charging
-            avg_power = vehicle.battery.load(self.interval, power)["avg_power"]
+            avg_power = vehicle.battery.load(self.interval, power, vehicle.desired_soc)["avg_power"]
             commands[cs_id] = avg_power
             available_power -= avg_power
             vehicles_to_charge -= 1
