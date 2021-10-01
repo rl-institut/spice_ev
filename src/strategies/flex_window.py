7041efacafad7370d982dc3f847359be3c43f3c8
stefan.schirmeister <stefan.schirmeister@localiser.de>
2021-10-01 09:55:36 +0200

    flex_window: basic greedy and needy (peak shaving)

--- src/strategies/flex_window.py
+++ src/strategies/flex_window.py
@@ -50,17 +50,9 @@ class FlexWindow(Strategy):
         avail_bat_power = sum([
             bat.get_available_power(self.interval) for bat in self.world_state.batteries.values()])
 
-        # dict to hold charging commands
-        charging_stations = {}
-
         # reset charging station power (nothing charged yet in this timestep)
         for cs in self.world_state.charging_stations.values():
             cs.current_power = 0
-
-        # order vehicles
-        vehicles = sorted(
-            [v for v in self.world_state.vehicles.values()
-                if v.connected_charging_station is not None], key=self.sort_key)
 
         cur_window = gc.window
         cur_feed_in = {k: -v for k, v in gc.current_loads.items() if v < 0}
@@ -107,25 +99,50 @@ class FlexWindow(Strategy):
                 ext_load = gc.get_avg_ext_load(cur_time, self.interval) - sum(cur_feed_in.values())
             timesteps.append({
                 "power": cur_max_power - ext_load,
+                "ext_load": ext_load,
                 "window": cur_window,
             })
 
-        # total_energy_needed = sum([v.get_energy_needed(full=True) for v in vehicles])
+        if util.get_cost(1, gc.cost) <= self.PRICE_THRESHOLD:
+            # charge max
+            commands = {}
+            for vehicle in self.world_state.vehicles.values():
+                cs_id = vehicle.connected_charging_station
+                if cs_id is None:
+                    continue
+                cs = self.world_state.charging_stations[cs_id]
+                p = gc.cur_max_power - gc.get_current_load()
+                p = util.clamp_power(p, vehicle, cs)
+                avg_power = vehicle.battery.load(self.interval, p)['avg_power']
+                commands[cs_id] = gc.add_load(cs_id, avg_power)
+                cs.current_power += avg_power
+        elif self.LOAD_STRAT == 'balanced':
+            commands = self.distribute_balanced(timesteps)
+        else:
+            commands = self.distribute_peak_shaving(timesteps)
+
+        """
+        # stationary batteries
+        for bid, battery in self.world_state.batteries.items():
+            if self.LOAD_STRAT == 'balanced':
+                # find minimum power to charge battery during windows
+        """
+        return {'current_time': self.current_time, 'commands': commands}
+
+    def distribute_balanced(self, timesteps):
+
+        gc = list(self.world_state.grid_connectors.values())[0]
+        # order vehicles
+        vehicles = sorted(
+            [v for v in self.world_state.vehicles.values()
+                if v.connected_charging_station is not None], key=self.sort_key)
+
+        # dict to hold charging commands
+        charging_stations = {}
 
         for vehicle in vehicles:
             cs_id = vehicle.connected_charging_station
             cs = self.world_state.charging_stations[cs_id]
-
-            if util.get_cost(1, gc.cost) <= self.PRICE_THRESHOLD:
-                # charge max
-                p = gc.cur_max_power - gc.get_current_load()
-                p = util.clamp_power(p, vehicle, cs)
-                avg_power = vehicle.battery.load(self.interval, p)['avg_power']
-                charging_stations[cs_id] = gc.add_load(cs_id, avg_power)
-                cs.current_power += avg_power
-                # no further computation for this vehicle in this timestep
-                continue
-
             sim_vehicle = deepcopy(vehicle)
             if self.LOAD_STRAT == 'balanced':
                 # simple case: charge balanced during windows
@@ -193,11 +210,118 @@ class FlexWindow(Strategy):
                 for ts_idx, ts_info in enumerate(timesteps):
                     ts_info["power"] -= power_vec[ts_idx]
 
-        """
-        # stationary batteries
-        for bid, battery in self.world_state.batteries.items():
-            if self.LOAD_STRAT == 'balanced':
-                # find minimum power to charge battery during windows
-        """
+        return charging_stations
 
-        return {'current_time': self.current_time, 'commands': charging_stations}
+    def distribute_peak_shaving(self, timesteps):
+
+        gc = list(self.world_state.grid_connectors.values())[0]
+        # order vehicles
+        vehicles = sorted(
+            [v for v in self.world_state.vehicles.values()
+                if v.connected_charging_station is not None], key=self.sort_key)
+
+        # what happens when all cars are charged with maximum power during charging windows?
+        sim_vehicles = deepcopy(vehicles)
+        cur_vehicles = sim_vehicles
+
+        cur_time = self.current_time - self.interval
+        for ts_info in timesteps:
+            cur_time += self.interval
+            cur_vehicles = [v for v in cur_vehicles if v.estimated_time_of_departure > cur_time]
+            cur_needed = sum([v.get_energy_needed(full=True) for v in cur_vehicles])
+            if not cur_vehicles or cur_needed < self.EPS:
+                # no cars or no energy need: skip check
+                break
+            self.distribute_power(cur_vehicles, ts_info["power"], cur_needed)
+        charged_in_window = all([v.get_delta_soc() < self.EPS for v in sim_vehicles])
+
+        if charged_in_window:
+            # can be charged within windows: reset SoC
+            for i, v in enumerate(sim_vehicles):
+                v.battery.soc = vehicles[i].battery.soc
+
+        old_soc = [v.battery.soc for v in sim_vehicles]
+
+        min_total_power = -gc.max_power
+        max_total_power = gc.max_power
+        # power_vec = [0]*len(timesteps)
+
+        safe = False
+
+        # while (charged_in_window and not safe) or max_total_power - min_total_power > self.EPS:
+        while max_total_power - min_total_power > self.EPS:
+            total_power = (min_total_power + max_total_power) / 2
+
+            # reset SoC
+            cur_vehicles = sim_vehicles
+            for i,v in enumerate(sim_vehicles):
+                v.battery.soc = old_soc[i]
+
+            cur_time = self.current_time - self.interval
+            for ts_info in timesteps:
+                cur_time += self.interval
+                new_cur_vehicles = []
+                for v in cur_vehicles:
+                    if v.estimated_time_of_departure > cur_time:
+                        new_cur_vehicles.append(v)
+                    elif v.get_delta_soc() > self.EPS:
+                        break
+                cur_vehicles = new_cur_vehicles
+                cur_needed = sum([v.get_energy_needed(full=True) for v in cur_vehicles])
+                if not cur_vehicles or cur_needed < self.EPS:
+                    # no cars or no energy need: skip simulation
+                    break
+                if ts_info["window"] == charged_in_window:
+                    self.distribute_power(
+                        cur_vehicles, total_power - ts_info["ext_load"], cur_needed)
+                elif not charged_in_window and ts_info["window"]:
+                    self.distribute_power(cur_vehicles, ts_info["power"], cur_needed)
+
+            safe = all([v.get_delta_soc() < self.EPS for v in sim_vehicles])
+
+            if safe:
+                max_total_power = total_power
+            else:
+                min_total_power = total_power
+
+        # apply power
+        total_energy_needed = sum([v.get_energy_needed(full=True) for v in vehicles])
+
+        # dict to hold charging commands
+        commands = {}
+        if gc.window == charged_in_window:
+            commands = self.distribute_power(
+                vehicles, total_power - gc.get_current_load(), total_energy_needed)
+        elif not charged_in_window and gc.window:
+            commands = self.distribute_power(
+                vehicles, gc.max_power - gc.get_current_load(), total_energy_needed)
+
+        for cs_id, power in commands.items():
+            commands[cs_id] = gc.add_load(cs_id, power)
+            # cs.current_power += power
+
+        # print(total_power, total_energy_needed, sum(commands.values()))
+        # raise Exception
+        return commands
+
+    def distribute_power(self, vehicles, total_power, total_needed):
+        commands = {}
+        power = 0
+        if total_power <= 0 or total_needed <= 0:
+            return {}
+
+        for v in vehicles:
+            cs_id = v.connected_charging_station
+            cs = self.world_state.charging_stations[cs_id]
+            if self.LOAD_STRAT == 'greedy':
+                power = total_power
+            elif self.LOAD_STRAT == 'needy':
+                energy_needed = v.get_energy_needed(full=True)
+                f = energy_needed / total_needed if total_needed > 0 else 0
+                power = f * total_power
+            else:
+                raise NotImplementedError
+            power = util.clamp_power(power, v, cs)
+            avg_power = v.battery.load(self.interval, power)["avg_power"]
+            commands[cs_id] = avg_power
+        return commands
