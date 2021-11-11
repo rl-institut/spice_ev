be6d8a29fcbb0e2472db61e2fd1108b5be0d19dd
Moritz Schiel <moritz.schiel@gmx.net>
2021-11-11 16:19:46 +0100

    add elmobile scripts

---
+++ elmobile_generate.py
@@ -0,0 +1,42 @@
+"""
+This script allows import of multiple SimBEV regions into SpiceEV scenarios.
+Author: Moritz
+"""
+
+from pathlib import Path
+from generate_from_simbev import generate_from_simbev
+from argparse import Namespace
+
+
+def generate_function(output, simbev, interval=15, price_seed=0, min_soc=0.5, min_soc_threshold=0.05,
+                      include_ext_load_csv="gesamtlast.csv", verbose=0, include_ext_csv_option=[],
+                      include_feed_in_csv="feedin_input_status_quo.csv", include_feed_in_csv_option=[], v2g_bool=False):
+    params = Namespace(output=output, simbev=simbev, interval=interval, price_seed=price_seed, min_soc=min_soc,
+                       include_ext_load_csv=include_ext_load_csv, include_ext_csv_option=include_ext_csv_option,
+                       include_feed_in_csv=include_feed_in_csv, include_feed_in_csv_option=include_feed_in_csv_option,
+                       include_price_csv=None, include_price_csv_option=[], min_soc_threshold=min_soc_threshold,
+                       verbose=verbose, config=None, eps=1e-10, use_simbev_soc=True, gc_power=1000000, v2g=v2g_bool)
+    generate_from_simbev(params)
+
+
+# input all data and call function here
+if __name__ == '__main__':
+    # set output directory; directory has to exist
+    output_dir = Path("elmobile_data", "status_quo_1")
+    output_dir.mkdir(exist_ok=True)
+    # set the simbev directory; ususally only the last folder name needs adjustment
+    simbev_dir = Path("..", "simbev", "simbev", "res", "elmobile_status_quo_2021-08-11_133042_simbev_run")
+    # set data csv file name, or None if not required
+    ext_load_csv = "gesamtlast.csv"
+    feed_in_csv = "feedin_input_status_quo.csv"
+
+    plz_list = [f.name for f in simbev_dir.iterdir() if f.is_dir()]
+    for counter, plz in enumerate(plz_list):
+        dirs = Path(simbev_dir, plz)
+        # time steps of the input csv are declared here (900s = 15mins, 3600s = 1h)
+        ext_csv_option = [["column", plz], ["step_duration_s", 900]]
+        feedin_csv_option = [["column", plz], ["step_duration_s", 3600]]
+        generate_function(Path(output_dir, plz + ".json"), dirs, include_ext_load_csv=ext_load_csv,
+                          include_ext_csv_option=ext_csv_option, include_feed_in_csv=feed_in_csv,
+                          include_feed_in_csv_option=feedin_csv_option)
+        print("Region {} done! ({}/{})".format(plz, counter+1, len(plz_list)))
---
+++ elmobile_simulate.py
@@ -0,0 +1,31 @@
+"""
+This script allows import of multiple SimBEV regions into SpiceEV scenarios.
+Author: Moritz
+"""
+
+from simulate import simulate
+from argparse import Namespace
+from pathlib import Path
+
+
+def simulate_function(input_file, output_file, result_file="results.json", strategy="balanced"):
+    """Call simulate with a complete Namespace
+    input_file: Path to scenario json as String
+    """
+    params = Namespace(input=input_file, strategy=strategy, margin=1,
+                       strategy_option=[("allow_negative_soc", True)], visual=False,
+                       save_timeseries=output_file, save_results=result_file, config=None, eta=False)
+    simulate(params)
+
+
+if __name__ == '__main__':
+    # set SpiceEV scenario directory
+    dirs = Path("elmobile_data", "status_quo_1")
+    plz_list = [f.stem for f in dirs.rglob("*.json")]
+    # set charging strategy
+    strat = "balanced"
+    result_dir = Path(dirs, "res_" + strat)
+    result_dir.mkdir(exist_ok=True)
+    for counter, plz in enumerate(plz_list):
+        simulate_function(Path(dirs, plz + ".json"), str(Path(result_dir, plz + ".csv")), strategy=strat)
+        print("Region {} done! ({}/{})".format(plz, counter+1, len(plz_list)))
