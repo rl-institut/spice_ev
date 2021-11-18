15034aed4b692e8cf44809d173e84a95cf702e49
jtm91 <jtm91@mailbox.tu-berlin.de>
2021-11-18 10:35:41 +0100

    Make flake happy

--- src/financial_calc.py
+++ src/financial_calc.py
@@ -19,8 +19,6 @@ class Financial:
             self.eeg_kwp_thr = setup['eeg']['selfconsumption_kWp_thr']
             self.eeg_self_cost = self.eeg_levy * setup['eeg']['selfconsumption_pct']
 
-
-
     def eeg_feed_in(self, energy, peak_power, ct=100):
         profit_ct = energy * look_up(peak_power, self.eeg_feed_in_points, type='step')
 
@@ -35,7 +33,7 @@ class Financial:
 
         return cost
 
-    def setup_smooth_capex(self, capex_dict):
+    def setup_capex(self, capex_dict):
         # TODO setup calculation that is compatible to smooth dictionaries
         pass
 
@@ -45,14 +43,17 @@ class Financial:
         # could potentially setup in class class function setup_smooth_capex
 
         self.battery_cost_dict[0] = {
-            "value": [c_rate, 2109.62368 / 1e3, -147.52325 / 1e6, 6.97016 / 1e9, -0.13996 / 1e12, 0.00102 / 1e15]}
+            "value": [c_rate, 2109.62368 / 1e3, -147.52325 / 1e6, 6.97016 / 1e9, -0.13996 / 1e12,
+                      0.00102 / 1e15]}
         self.battery_cost_dict[50] = {"value": [c_rate, 1000.2 / 1e3, -0.4983 / 1e6]}
         self.battery_cost_dict[1000] = {"value": [capacity * self.battery_scale, 0.353, 0.149]}
 
         if capacity < 50:
-            cost = get_cost(capacity * self.battery_scale, {**self.battery_cost_dict[0], **self.battery_cost_type})
+            cost = get_cost(capacity * self.battery_scale,
+                            {**self.battery_cost_dict[0], **self.battery_cost_type})
         elif capacity < 1000:
-            cost = get_cost(capacity * self.battery_scale, {**self.battery_cost_dict[50], **self.battery_cost_type})
+            cost = get_cost(capacity * self.battery_scale,
+                            {**self.battery_cost_dict[50], **self.battery_cost_type})
         elif capacity >= 1000:
             # TODO: last intervall has offset/scaling issue
             cost = get_cost(1, {**self.battery_cost_dict[1000], **self.battery_cost_type})
@@ -63,7 +64,7 @@ class Financial:
 
     def grid_cost(self, sc):
         # TODO
-        return [(i, val.max_power) for i, val  in sc.constants.grid_connectors.items()]
+        return [(i, val.max_power) for i, val in sc.constants.grid_connectors.items()]
 
     def capex_cost(self, sc):
         # TODO
