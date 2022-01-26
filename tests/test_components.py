b69ba7dc0560814e585d3507ff97b669c213afc2
Piranias <inia.steinbach@rl-institut.de>
2022-01-26 10:31:17 +0100

    remove generate tests from components

--- tests/test_components.py
+++ tests/test_components.py
@@ -5,53 +5,6 @@ import unittest
 from src import battery, loading_curve, scenario
 
 
-def get_test_json():
-    # get minimum working json example
-    return {
-        "scenario": {
-            "start_time": "2020-01-01T00:00:00+02:00",
-            "interval": 15,
-            "n_intervals": 35040
-        },
-        "constants": {
-            "grid_connectors": {},
-            "charging_stations": {},
-            "vehicle_types": {},
-            "vehicles": {},
-        },
-        "events": {
-            "external_loads": {},
-            "grid_operator_signals": [],
-            "vehicle_events": [],
-        },
-        "strategy": "greedy"
-    }
-
-
-class TestScenario(unittest.TestCase):
-
-    def test_scenario_times(self):
-        # corect number of timesteps?
-        j = get_test_json()
-        s = scenario.Scenario(j)
-        self.assertEqual(s.n_intervals, 35040)
-
-        # either n_intervals or stop time, not both
-        j['scenario']['stop_time'] = "2020-01-01T01:00:00+02:00"
-        with self.assertRaises(AssertionError):
-            s = scenario.Scenario(j)
-
-        # remove n_intervals, stop time remains: success, four timesteps
-        del j['scenario']['n_intervals']
-        s = scenario.Scenario(j)
-        self.assertEqual(s.n_intervals, 4)
-
-    def test_file(self):
-        # open from file
-        with open('test_data/scenario.json', 'r') as f:
-            scenario.Scenario(json.load(f), "")
-
-
 def approx_eq(x, y, eps=1e-3):
     return abs(x - y) < eps
 
