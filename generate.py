18017184272f42a1bb35f78959882ec1c491720e
Piranias <inia.steinbach@rl-institut.de>
2022-02-02 10:53:13 +0100

    correct argparser help text

--- generate.py
+++ generate.py
@@ -370,9 +370,9 @@ if __name__ == '__main__':
                         help='set duration of scenario as number of days')
     parser.add_argument('--interval', metavar='MIN', type=int, default=15,
                         help='set number of minutes for each timestep (Δt)')
-    parser.add_argument('--start-time', default='2018-01-01T00:15:00+00:00',
-                        help='Provide start time of simulation in ISO format YYYY-MM-DDTHH:MM:SS. '
-                             'Precision is 1 second. E.g. 2018-01-31T00:15:00')
+    parser.add_argument('--start-time', default='2018-01-01T01:00:00+00:00',
+                        help='Provide start time of simulation in ISO format YYYY-MM-DDTHH:MM:SS.'
+                             'Precision is 1 second. E.g. 2018-01-31T01:00:00')
     parser.add_argument('--min-soc', metavar='SOC', type=float, default=0.8,
                         help='set minimum desired SOC (0 - 1) for each charging process')
     parser.add_argument('--battery', '-b', default=[], nargs=2, type=float, action='append',
