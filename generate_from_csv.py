872e04a7f8d07178f01675071b2bdb88b7b43302
Piranias <inia.steinbach@rl-institut.de>
2022-02-22 15:48:37 +0100

    remove buffer and fix delta_soc as negative value

--- generate_from_csv.py
+++ generate_from_csv.py
@@ -143,9 +143,6 @@ def generate_from_csv(args):
             else:
                 delta_soc = float(row["delta_soc"])
 
-            desired_soc = delta_soc * (1 + vars(args).get("buffer", 0.1))
-            desired_soc = max(args.min_soc, desired_soc)
-
             events["vehicle_events"].append({
                 "signal_time": arrival.isoformat(),
                 "start_time": arrival.isoformat(),
@@ -154,7 +151,7 @@ def generate_from_csv(args):
                 "update": {
                     "connected_charging_station": "CS_" + v_name,
                     "estimated_time_of_departure": departure.isoformat(),
-                    "soc_delta": delta_soc,
+                    "soc_delta": -delta_soc,
                 }
             })
 
@@ -171,7 +168,6 @@ def generate_from_csv(args):
                     "event_type": "departure",
                     "update": {
                         "estimated_time_of_arrival":  next_arrival.isoformat(),
-                        "desired_soc": desired_soc,
                     }
                 })
 
