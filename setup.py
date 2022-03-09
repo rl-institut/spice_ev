d3bede1d2483a28b04dee3bc3f8f4d78c0765a19
Piranias <inia.steinbach@rl-institut.de>
2022-03-09 09:30:50 +0100

    change name of spice_ev

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="spiceEV",
+    name="spice_ev",
     version="0.0.0",
     description="Simulation Program for Individual Charging Events of Electric Vehicles.",
     url="https://github.com/rl-institut/spice_ev",
     author="Reiner Lemoine Institut",
     # author_email='sabine.haas@rl-institut.de',
     license="MIT",
-    packages= find_packages(),
+    packages=find_packages(),
     package_data={},
-    #long_description=read("README.rst"),
-    #long_description_content_type="text/x-rst",
-    #python_requires=">=3.5, <4",
-    #install_requires=[],
-    #extras_require={},
 )
