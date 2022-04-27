f8570ccc1b8fcf0312e2f5f03cfe422fc68736de
Paul Boehnke <paul.boehnke@rl-institut.de>
2022-04-27 15:05:57 +0200

    allow for loading spice_ev as package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spiceev",
+    version="0.0.1dev",
+    description="Simulation Program for Individual Charging Events of Electric Vehicles.",
+    url="https://github.com/rl-institut/spice_ev",
+    author="Reiner Lemoine Institut",
+    # author_email='sabine.haas@rl-institut.de',
+    license="MIT",
+    packages=find_packages(),
+    package_data={},
+)
