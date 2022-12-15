49467fe69883d2ad9e0c46338d028efd257dab52
Jakob Gemassmer <49757031+jakobgemassmer@users.noreply.github.com>
2022-12-15 11:07:34 +0100

    Update setup.py
    adjust setup author name and version number

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="spiceev",
-    version="0.0.1dev",
+    version="1.0.0dev",
     description="Simulation Program for Individual Charging Events of Electric Vehicles.",
     url="https://github.com/rl-institut/spice_ev",
     author="Reiner Lemoine Institut",
-    # author_email='sabine.haas@rl-institut.de',
+    author_email='info@rl-institut.de',
     license="MIT",
     packages=find_packages(),
     package_data={},
